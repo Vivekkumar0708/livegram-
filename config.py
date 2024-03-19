bc56bb612b50a924a6b74f89eb0a47728a9b9bcf
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-19 16:42:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ TOKEN = getenv("TOKEN", None)
 DB_URI = getenv("MONGO_URL")
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
+USER_MODE=getenv("USER_MODE",False)
+#if you want use string session then turn on this and put you session down
+SESSION = getenv("SESSION","")

65e0699eb6244a319341556bc647750f661d6693
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-20 12:03:38 +0530

    Update __init__.py

--- Assistant/__init__.py
+++ Assistant/__init__.py
@@ -25,7 +25,7 @@ class App(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             lang_code="en",
-            session_string=config.TOKEN,
+            bot_token=config.TOKEN,
             plugins=dict(root="Assistant.modules"),
             in_memory=True,
             parse_mode=enums.ParseMode.DEFAULT,

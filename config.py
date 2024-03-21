85727e7a28fe6db4b3f33c75f3bba429d1f5339b
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-21 10:11:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = getenv("API_ID", 25488022)
-API_HASH = getenv("API_HASH", "0c999a454fddd79251213be7944811e8")
-TOKEN = getenv("TOKEN", "7146371782:AAEVQ9SUWYxnpWgME3CecKJEetiPIufKFyY")
-DB_URI = getenv("MONGO_URL","mongodb+srv://Lucifer121:vivek@jaishreeram.z0o2fuy.mongodb.net/?retryWrites=true&w=majority")
-LOGGER_ID = int(getenv("LOGGER_ID", -1002042572827))
+API_ID = getenv("API_ID", )
+API_HASH = getenv("API_HASH", "")
+TOKEN = getenv("TOKEN", "")
+DB_URI = getenv("MONGO_URL","")
+LOGGER_ID = int(getenv("LOGGER_ID", ))
 SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
